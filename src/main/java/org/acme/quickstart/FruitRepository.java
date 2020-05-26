7934c7f95db9c383606ad80abd4f69103a47889e
Niemann, Markus <markus.niemann@lhind.dlh.de>
2020-05-26 14:55:36 +0200

    initial commit

---
+++ src/main/java/org/acme/quickstart/FruitRepository.java
@@ -0,0 +1,9 @@
+package org.acme.quickstart;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FruitRepository extends CrudRepository<Fruit, Long> {
+
+  List<Fruit> findByColor(String color);
+}
---
+++ src/main/java/org/acme/quickstart/StringFunction.java
@@ -0,0 +1,5 @@
+package org.acme.quickstart;
+
+import java.util.function.Function;
+
+public interface StringFunction extends Function<String, String> {}
